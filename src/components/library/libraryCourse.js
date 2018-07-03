2a67e1003e819148213d1d7c9a858354a7dded07
Words1223 <amp069597@gmail.com>
2018-07-03 11:21:21 -0600

    fixed grid and added arrow component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 
 import Icon from '../icon';
+import Arrow from '../arrow';
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <label classsName="library-course__title">Problem Solving</label>
-                { Icon('fas fa-check', 'library-course__icon') }
-                {/* arrow component */}
+                <div className="library-course__title-arrow">
+                    <label classsName="library-course__title">Problem Solving</label>
+                    { Icon('fas fa-check', 'library-course__icon') }
+                </div>
+                <Arrow className="library-course__arrow"/>
                 {/* action button component */}
                 <div classsName="library-course__description">
                     <label>Course Description</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id tortor lacinia, luctus leo ultrices, gravida dolor. Aliquam a scelerisque magna, a mollis metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin vestibulum finibus ipsum. Cras id orci a metus laoreet rhoncus eu ullamcorper nisi. </p>
+                    <p>
+                       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id tortor lacinia, luctus leo ultrices,
+                       gravida dolor. Aliquam a scelerisque magna, a mollis metus. Pellentesque habitant morbi tristique senectus 
+                       et netus et malesuada fames ac turpis egestas. Proin vestibulum finibus ipsum. Cras id orci a metus laoreet 
+                       rhoncus eu ullamcorper nisi. 
+                    </p>
                 </div>
             </div>
-        )
+        );
     }
 }
 
